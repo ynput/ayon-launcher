aa0787b09bcba6f2a791b8a6302e57bfbef84f3a
Ondrej Samohel <ondrej.samohel@ynput.io>
2023-08-24 11:01:29 +0200

    :bug: fix docker build on centos 7

--- tools/docker_build.sh
+++ tools/docker_build.sh
@@ -21,11 +21,11 @@ realpath () {
 }
 
 create_container () {
-  if [ ! -f "$openpype_root/build/docker-image.id" ]; then
+  if [ ! -f "$launcher_root/build/docker-image.id" ]; then
     echo -e "${BIRed}!!!${RST} Docker command failed, cannot find image id."
     exit 1
   fi
-  local id=$(<"$openpype_root/build/docker-image.id")
+  local id=$(<"$launcher_root/build/docker-image.id")
   echo -e "${BIYellow}---${RST} Creating container from $id ..."
   cid="$(docker create $id bash)"
   if [ $? -ne 0 ] ; then
@@ -36,18 +36,18 @@ create_container () {
 
 retrieve_build_log () {
   create_container
-  echo -e "${BIYellow}***${RST} Copying build log to ${BIWhite}$openpype_root/build/build.log${RST}"
-  docker cp "$cid:/opt/openpype/build/build.log" "$openpype_root/build"
+  echo -e "${BIYellow}***${RST} Copying build log to ${BIWhite}$launcher_root/build/build.log${RST}"
+  docker cp "$cid:/opt/ayon-launcher/build/build.log" "$launcher_root/build"
 }
 
-openpype_root=$(realpath $(dirname $(dirname "${BASH_SOURCE[0]}")))
+launcher_root=$(realpath $(dirname $(dirname "${BASH_SOURCE[0]}")))
 
 
 if [ -z $1 ]; then
     dockerfile="Dockerfile"
 else
   dockerfile="Dockerfile.$1"
-  if [ ! -f "$openpype_root/$dockerfile" ]; then
+  if [ ! -f "$launcher_root/$dockerfile" ]; then
     echo -e "${BIRed}!!!${RST} Dockerfile for specifed platform ${BIWhite}$1${RST} doesn't exist."
     exit 1
   else
@@ -57,18 +57,18 @@ fi
 
 # Main
 main () {
-  openpype_root=$(realpath $(dirname $(dirname "${BASH_SOURCE[0]}")))
-  pushd "$openpype_root" > /dev/null || return > /dev/null
+  launcher_root=$(realpath $(dirname $(dirname "${BASH_SOURCE[0]}")))
+  pushd "$launcher_root" > /dev/null || return > /dev/null
 
   echo -e "${BIYellow}---${RST} Cleaning build directory ..."
-  rm -rf "$openpype_root/build" && mkdir "$openpype_root/build" > /dev/null
+  rm -rf "$launcher_root/build" && mkdir "$launcher_root/build" > /dev/null
 
-  local version_command="import os;exec(open(os.path.join('$openpype_root', 'openpype', 'version.py')).read());print(__version__);"
-  local openpype_version="$(python3 <<< ${version_command})"
+  local version_command="import os;exec(open(os.path.join('$launcher_root', 'version.py')).read());print(__version__);"
+  local launcher_version="$(python <<< ${version_command})"
 
   echo -e "${BIGreen}>>>${RST} Running docker build ..."
-  # docker build --pull --no-cache -t pypeclub/openpype:$openpype_version .
-  docker build --pull --iidfile $openpype_root/build/docker-image.id --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') --build-arg VERSION=$openpype_version -t pypeclub/openpype:$openpype_version -f $dockerfile .
+  # docker build --pull --no-cache -t pypeclub/openpype:$launcher_version .
+  docker build --pull --iidfile $launcher_root/build/docker-image.id --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') --build-arg VERSION=$launcher_version -t ynput/ayon_launcher:$launcher_version -f $dockerfile .
   if [ $? -ne 0 ] ; then
     echo $?
     echo -e "${BIRed}!!!${RST} Docker build failed."
@@ -79,8 +79,8 @@ main () {
   echo -e "${BIGreen}>>>${RST} Copying build from container ..."
   create_container
   echo -e "${BIYellow}---${RST} Copying ..."
-  docker cp "$cid:/opt/openpype/build/exe.linux-x86_64-3.9" "$openpype_root/build"
-  docker cp "$cid:/opt/openpype/build/build.log" "$openpype_root/build"
+  docker cp "$cid:/opt/ayon-launcher/build/output" "$launcher_root/build"
+  docker cp "$cid:/opt/ayon-launcher/build/build.log" "$launcher_root/build"
   if [ $? -ne 0 ] ; then
     echo -e "${BIRed}!!!${RST} Copying failed."
     return 1
