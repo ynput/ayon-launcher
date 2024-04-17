fefeb4f22f0d9e07806feb2c9c4fa07a74d05194
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2024-04-17 13:51:26 +0200

    base implementation of shim

---
+++ shim/setup.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+"""Setup info for building AYON Desktop application."""
+import os
+import platform
+from pathlib import Path
+
+from cx_Freeze import setup, Executable
+
+ayon_root = Path(os.path.dirname(__file__))
+resources_dir = ayon_root.parent / "common" / "ayon_common" / "resources"
+
+__version__ = "1.0.0"
+
+low_platform_name = platform.system().lower()
+IS_WINDOWS = low_platform_name == "windows"
+IS_LINUX = low_platform_name == "linux"
+IS_MACOS = low_platform_name == "darwin"
+
+base = None
+if IS_WINDOWS:
+    base = "Win32GUI"
+
+include_files = [
+    "version",
+]
+
+icon_path = resources_dir / "AYON.ico"
+mac_icon_path = resources_dir / "AYON.icns"
+
+build_exe_options = dict(
+    build_exe="dist",
+    optimize=2,
+    replace_paths=[("*", "")],
+    include_files=include_files,
+    excludes=[
+        "_socket",
+        "asyncio",
+        "concurrent",
+        "email",
+        "html",
+        "http",
+        "lib2to3",
+        "libcrypto",
+        "libssl",
+        "multiprocessing",
+        "pydoc_data",
+        "select",
+        "sqlite3",
+        "test",
+        "tkinter",
+        "unittest",
+        "urllib",
+        "xmlrps",
+    ]
+)
+
+bdist_mac_options = dict(
+    bundle_name=f"AYON shim {__version__}",
+    iconfile=mac_icon_path
+)
+
+executables = [
+    Executable(
+        "shim_start.py",
+        base=base,
+        target_name="ayon",
+        icon=icon_path.as_posix()
+    ),
+]
+if IS_WINDOWS:
+    executables.append(
+        Executable(
+            "shim_start.py",
+            base=None,
+            target_name="ayon_console",
+            icon=icon_path.as_posix()
+        )
+    )
+
+setup(
+    name="AYON-shim",
+    version=__version__,
+    description="AYON Desktop Client shim",
+    options={
+        "build_exe": build_exe_options,
+        "bdist_mac": bdist_mac_options,
+    },
+    executables=executables,
+    packages=[]
+)
---
+++ shim/shim_start.py
@@ -0,0 +1,191 @@
+import os
+import platform
+import sys
+import json
+import subprocess
+
+import appdirs
+import semver
+
+IS_BUILT_APPLICATION = getattr(sys, "frozen", False)
+CURRENT_PATH = os.path.abspath(__file__)
+
+
+def get_ayon_appdirs(*args):
+    """Local app data directory of AYON launcher.
+
+    Args:
+        *args (Iterable[str]): Subdirectories/files in local app data dir.
+
+    Returns:
+        str: Path to directory/file in local app data dir.
+    """
+
+    return os.path.join(
+        appdirs.user_data_dir("AYON", "Ynput"),
+        *args
+    )
+
+
+# Store executables info to a file
+def get_executables_info_filepath():
+    """Get path to file where information about executables is stored.
+
+    Returns:
+        str: Path to json file where executables info are stored.
+    """
+
+    return get_ayon_appdirs("executables.json")
+
+
+def get_executables_info():
+    filepath = get_executables_info_filepath()
+    data = {
+        "file_version": "1.0.1",
+        "available_versions": []
+    }
+    if os.path.exists(filepath):
+        try:
+            with open(filepath, "r") as stream:
+                data = json.load(stream)
+
+        except Exception:
+            pass
+
+    return data
+
+
+def load_version_from_file(filepath):
+    """Execute python file and return '__version__' variable."""
+
+    with open(filepath, "r") as stream:
+        version_content = stream.read()
+    version_globals = {}
+    exec(version_content, version_globals)
+    return version_globals["__version__"]
+
+
+def load_version_from_root(root):
+    """Get version of executable.
+
+    Args:
+        root (str): Path to executable.
+
+    Returns:
+        Union[str, None]: Version of executable.
+    """
+
+    version = None
+    if not root or not os.path.exists(root):
+        return version
+
+    version_filepath = os.path.join(root, "version.py")
+    if os.path.exists(version_filepath):
+        try:
+            version = load_version_from_file(version_filepath)
+        except Exception as exc:
+            print("Failed lo load version file {}. {}".format(
+                version_filepath, exc))
+
+    return version
+
+
+def load_executable_version(executable):
+    """Get version of executable.
+
+    Args:
+        executable (str): Path to executable.
+
+    Returns:
+        Union[str, None]: Version of executable.
+    """
+
+    if not executable:
+        return None
+    return load_version_from_root(os.path.dirname(executable))
+
+
+class Executable:
+    def __init__(self, path, version):
+        self.path = path
+        self.version = version
+        self._exists = None
+        self._semver_version = None
+
+    def __repr__(self):
+        return f"AYON Executable ({self.version}): '{self.path}'"
+
+    def __eq__(self, other):
+        return self.semver_version == other.semver_version
+
+    def __lt__(self, other):
+        return self.semver_version < other.semver_version
+
+    def __gt__(self, other):
+        return self.semver_version > other.semver_version
+
+    def __ge__(self, other):
+        return self.semver_version >= other.semver_version
+
+    def __le__(self, other):
+        return self.semver_version <= other.semver_version
+
+    @property
+    def exists(self):
+        if self._exists is None:
+            self._exists = os.path.exists(self.path)
+        return self._exists
+
+    @property
+    def semver_version(self):
+        if self._semver_version is None:
+            try:
+                self._semver_version = semver.VersionInfo.parse(self.version)
+            except Exception:
+                self._semver_version = semver.VersionInfo.parse("0.0.0")
+        return self._semver_version
+
+
+def main():
+    executables_info = get_executables_info()
+
+    executables = []
+    for version_info in executables_info["available_versions"]:
+        executable = version_info["executable"]
+        version = load_executable_version(executable)
+        executable = Executable(executable, version)
+        if executable.exists:
+            executables.append(executable)
+
+    if not executables:
+        raise RuntimeError(
+            "Shim was not able to locate any AYON launcher executables."
+        )
+    executables.sort()
+    executable = executables[-1]
+
+    # Split dir and filename
+    # - decide if filename should be 'ayon_console.exe' for windows
+    #   other platforms have only 'ayon' executable
+    executable_dir, executable_filename = os.path.split(executable.path)
+    if (
+        platform.system().lower() == "windows"
+        and (
+            not IS_BUILT_APPLICATION
+            or "ayon_console" in os.path.basename(sys.argv[0])
+        )
+    ):
+        executable_filename = "ayon_console.exe"
+
+    executable_path = os.path.join(executable_dir, executable_filename)
+
+    # Change first argument to executable path
+    # - replace shim executable when running from build
+    # - replace start python script path when running from code
+    args = list(sys.argv)
+    args[0] = executable_path
+    os.system(subprocess.list2cmdline(args))
+
+
+if __name__ == "__main__":
+    main()
