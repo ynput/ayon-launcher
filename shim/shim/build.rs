985525a8043f7c93f73db4ca5c688bc6a600b1a5
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2026-04-09 16:26:12 +0200

    fix resources dir

--- shim/shim/build.rs
+++ shim/shim/build.rs
@@ -2,7 +2,7 @@ fn main() {
     #[cfg(target_os = "windows")]
     {
         let mut res = winres::WindowsResource::new();
-        res.set_icon("../../../common/ayon_common/resources/AYON.ico");
+        res.set_icon("../../common/ayon_common/resources/AYON.ico");
         res.compile().unwrap();
     }
 }
