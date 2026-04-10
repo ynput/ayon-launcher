fb76eb0911f51ff3d395a590e2b9c9f80ed726e8
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2026-04-10 15:51:56 +0200

    removed unnecessary build.rs

--- shim/build.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    #[cfg(target_os = "windows")]
-    {
-        let mut res = winres::WindowsResource::new();
-        res.set_icon("../../common/ayon_common/resources/AYON.ico");
-        res.compile().unwrap();
-    }
-}
